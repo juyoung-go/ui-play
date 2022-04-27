fc9bb0532f55cc992401fe8c65dee2866070b4af
juyoung-go <juyounggo@rsqaure.co.kr>
2022-04-27 18:33:07 +0900

    dashboard test added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <select v-model="comName" style="height:40px;margin-right:5px;" @change="changeMode()">
-      <template v-for="(opt, i) in comList">
-        <option :key="i" :value="opt.value">{{opt.label}}</option>
-      </template>
-    </select>
+    <div style="height:40px;display:flex;justify-content:space-between;align-items:center;padding:20px 5px;">
+      <div style="margin-left:10px;border:1px solid gray;border-radius:5px;font-size:16px;padding:5px;height:30px;background:lightgreen;">My UI Playground</div>
+      <div>
+        <select v-model="comName" style="margin-right:5px;" @change="changeMode()">
+          <template v-for="(opt, i) in comList">
+            <option :key="i" :value="opt.value">{{opt.label}}</option>
+          </template>
+        </select>
+      </div>
+      <div></div>
+    </div>
     <component v-if="comName" :is="$options.components[comName]" style="height:calc(100% - 40px);"></component>
   </div>
 </template>
@@ -13,12 +19,12 @@
 
 import PagingTest from './components/PagingTest.vue'
 import MapTest from './components/MapTest.vue'
-import CanvasTest from './components/CanvasTest.vue'
+import DashboardTest from './components/DashboardTest.vue'
 import MapComponentTest from './components/MapComponentTest.vue'
 export default {
   name: 'App',
   components:{
-    PagingTest, MapTest, CanvasTest, MapComponentTest
+    PagingTest, MapTest, DashboardTest, MapComponentTest
   },
   created(){
     console.log(this.$options.components);
@@ -36,7 +42,7 @@ export default {
   data(){
     return {
       comList:[],
-      comName:'MapComponentTest',
+      comName:'PagingTest',
     }
   }
 }
---
+++ src/components/DashboardTest.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="root" style="width:100vw;height:100vh;">
+  </div>
+</template>
+
+<script>
+export default {
+  name:'CanvasTest',
+  async created(){
+    await this.$nextTick()
+    const root = this.$refs.root
+    const foo = document.createElement('iframe');
+    const mapUrl = "http://localhost:5601/app/dashboards#/view/011b33d0-b4ab-11ec-a535-5bcc1a0d7fdb?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))"
+    foo.setAttribute("src", mapUrl);
+    foo.setAttribute("width", root.offsetWidth - 100);
+    foo.setAttribute("height", root.offsetHeight - 50);
+    foo.style.border = '0px'
+    root.appendChild(foo)
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
