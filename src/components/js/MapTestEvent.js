4192ed50f31b49dfa1f54a8f16e7925e4cc66e0e
goju2k <goju2k@naver.com>
2022-11-09 00:33:35 +0900

    canvas map 모드에서 mouseup 일때는 마커 위치 재계산하지 않도록 수정 (성능향상)

--- src/components/js/MapTestEvent.js
+++ src/components/js/MapTestEvent.js
@@ -83,7 +83,7 @@ export default {
     if(this.mode == 'canvas'){
       this.clearMarker()
       this.ctx.translate(trans.x, trans.y)
-      this.drawMarker2(true)
+      this.drawMarker2()
     }
     
   },
